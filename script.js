bcc9f29372b7301e57f2821c17758a7445e2acb0
Lucas klafke <lucasnkz11@gmail.com>
2022-02-24 14:40:05 -0300

    select only one object done and verify input link done

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ function caseError(){
 }
 getProducts()
 
-
 function renderProducts(infos){
     const productContainer = document.querySelector(".product-container")
     productContainer.innerHTML = ''
@@ -27,4 +26,36 @@ function renderProducts(infos){
         </div>
     `
 }
-setInterval(getProducts,3000)
+function selectOption(element){
+    let container = element.parentNode
+    let arrayContainer = container.querySelectorAll(".option-container")
+    const childrenList = element.children
+    for(i = 0; i < arrayContainer.length;i++){
+        let children = arrayContainer[i].children
+        for(c = 0; c < childrenList.length;c++){
+            if (children[c].classList.contains("selected")){
+                children[c].classList.remove("selected")
+            }
+        }
+    }
+    for(i = 0; i < childrenList.length;i++){
+        if(childrenList[i].classList.contains("circle-background")){
+            childrenList[i].classList.add("selected")
+        }
+    }
+}
+function verifyInput(){
+    console.log("entrei")
+    const input = document.querySelector(".image-reference input")
+    const link = input.value
+    const https = /^https/.test(link)
+    const png = /.png$/.test(link)
+    const jpg = /.jpg$/.test(link)
+    if (https){
+        if (png || jpg){
+            postProduct(link)
+        }
+    }
+}
+
+// setInterval(getProducts,3000)
